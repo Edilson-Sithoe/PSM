0c4bb0708166995f54171e4a134f3575d1f8699e
Edilson-Sithoe <elisa@19751219>
2021-11-17 19:03:35 +0200

    2021_11_17 Updaate

---
+++ app/src/main/java/projecto/jhpiego/psm/model/FreePrevAnFFS.java
@@ -0,0 +1,515 @@
+package projecto.jhpiego.psm.model;
+
+public class FreePrevAnFFS {
+    private String chkYesPO;
+    private String chkNoPO;
+
+    private String chkYesEC;
+    private String chkNoEC;
+
+    private String chkYesFE;
+    private String chkNoFE;
+
+    private String chkLGE;
+    private String chkBC;
+    private String chkABC;
+    private String chkCO;
+    private String chkD;
+    private String chkK;
+    private String chkDontNo;
+    private String txtOther;
+
+    private String chkLessMD;
+    private String chkMoreThaOnYMD;
+    private String chkMoreThnTwoYMD;
+    private String chkDontNoMD;
+
+    private String chkYesAS;
+    private String chkNoAS;
+
+    private String chkYesSW;
+    private String chkNoSW;
+    private String chkDontNoSW;
+
+    private String chkLessLMD;
+    private String chkMoreThaOnYLMD;
+    private String chkMoreThnTwoYLMD;
+    private String chkDontNoLMD;
+
+    private String chkYesHS;
+    private String chkNoHS;
+
+    private String chkYesSWT;
+    private String chkNoSWT;
+    private String chkDontNoSWT;
+
+    private String chkLessLMDT;
+    private String chkMoreThaOnYLMDT;
+    private String chkMoreThnTwoYLMDT;
+    private String chkDontNoLMDT;
+
+    private String chkYesCS;
+    private String chkNoCS;
+
+    private String chkYesSWTH;
+    private String chkNoSWTH;
+    private String chkDontNoSWTH;
+
+    private String chkLessLMDTH;
+    private String chkMoreThaOnYLMDTH;
+    private String chkMoreThnTwoYLMDTH;
+    private String chkDontNoLMDTH;
+
+    private String chkYesWTF;
+    private String chkNoWTF;
+    private String chkDontNoWTF;
+
+    private String txtCommnts;
+
+    public FreePrevAnFFS() {
+    }
+
+    public FreePrevAnFFS(String chkYesPO, String chkNoPO, String chkYesEC, String chkNoEC, String chkYesFE, String chkNoFE, String chkLGE, String chkBC, String chkABC, String chkCO, String chkD, String chkK, String chkDontNo, String txtOther, String chkLessMD, String chkMoreThaOnYMD, String chkMoreThnTwoYMD, String chkDontNoMD, String chkYesAS, String chkNoAS, String chkYesSW, String chkNoSW, String chkDontNoSW, String chkLessLMD, String chkMoreThaOnYLMD, String chkMoreThnTwoYLMD, String chkDontNoLMD, String chkYesHS, String chkNoHS, String chkYesSWT, String chkNoSWT, String chkDontNoSWT, String chkLessLMDT, String chkMoreThaOnYLMDT, String chkMoreThnTwoYLMDT, String chkDontNoLMDT, String chkYesCS, String chkNoCS, String chkYesSWTH, String chkNoSWTH, String chkDontNoSWTH, String chkLessLMDTH, String chkMoreThaOnYLMDTH, String chkMoreThnTwoYLMDTH, String chkDontNoLMDTH, String chkYesWTF, String chkNoWTF, String chkDontNoWTF, String txtCommnts) {
+        this.chkYesPO = chkYesPO;
+        this.chkNoPO = chkNoPO;
+        this.chkYesEC = chkYesEC;
+        this.chkNoEC = chkNoEC;
+        this.chkYesFE = chkYesFE;
+        this.chkNoFE = chkNoFE;
+        this.chkLGE = chkLGE;
+        this.chkBC = chkBC;
+        this.chkABC = chkABC;
+        this.chkCO = chkCO;
+        this.chkD = chkD;
+        this.chkK = chkK;
+        this.chkDontNo = chkDontNo;
+        this.txtOther = txtOther;
+        this.chkLessMD = chkLessMD;
+        this.chkMoreThaOnYMD = chkMoreThaOnYMD;
+        this.chkMoreThnTwoYMD = chkMoreThnTwoYMD;
+        this.chkDontNoMD = chkDontNoMD;
+        this.chkYesAS = chkYesAS;
+        this.chkNoAS = chkNoAS;
+        this.chkYesSW = chkYesSW;
+        this.chkNoSW = chkNoSW;
+        this.chkDontNoSW = chkDontNoSW;
+        this.chkLessLMD = chkLessLMD;
+        this.chkMoreThaOnYLMD = chkMoreThaOnYLMD;
+        this.chkMoreThnTwoYLMD = chkMoreThnTwoYLMD;
+        this.chkDontNoLMD = chkDontNoLMD;
+        this.chkYesHS = chkYesHS;
+        this.chkNoHS = chkNoHS;
+        this.chkYesSWT = chkYesSWT;
+        this.chkNoSWT = chkNoSWT;
+        this.chkDontNoSWT = chkDontNoSWT;
+        this.chkLessLMDT = chkLessLMDT;
+        this.chkMoreThaOnYLMDT = chkMoreThaOnYLMDT;
+        this.chkMoreThnTwoYLMDT = chkMoreThnTwoYLMDT;
+        this.chkDontNoLMDT = chkDontNoLMDT;
+        this.chkYesCS = chkYesCS;
+        this.chkNoCS = chkNoCS;
+        this.chkYesSWTH = chkYesSWTH;
+        this.chkNoSWTH = chkNoSWTH;
+        this.chkDontNoSWTH = chkDontNoSWTH;
+        this.chkLessLMDTH = chkLessLMDTH;
+        this.chkMoreThaOnYLMDTH = chkMoreThaOnYLMDTH;
+        this.chkMoreThnTwoYLMDTH = chkMoreThnTwoYLMDTH;
+        this.chkDontNoLMDTH = chkDontNoLMDTH;
+        this.chkYesWTF = chkYesWTF;
+        this.chkNoWTF = chkNoWTF;
+        this.chkDontNoWTF = chkDontNoWTF;
+        this.txtCommnts = txtCommnts;
+    }
+
+    public String getChkYesPO() {
+        return chkYesPO;
+    }
+
+    public void setChkYesPO(String chkYesPO) {
+        this.chkYesPO = chkYesPO;
+    }
+
+    public String getChkNoPO() {
+        return chkNoPO;
+    }
+
+    public void setChkNoPO(String chkNoPO) {
+        this.chkNoPO = chkNoPO;
+    }
+
+    public String getChkYesEC() {
+        return chkYesEC;
+    }
+
+    public void setChkYesEC(String chkYesEC) {
+        this.chkYesEC = chkYesEC;
+    }
+
+    public String getChkNoEC() {
+        return chkNoEC;
+    }
+
+    public void setChkNoEC(String chkNoEC) {
+        this.chkNoEC = chkNoEC;
+    }
+
+    public String getChkYesFE() {
+        return chkYesFE;
+    }
+
+    public void setChkYesFE(String chkYesFE) {
+        this.chkYesFE = chkYesFE;
+    }
+
+    public String getChkNoFE() {
+        return chkNoFE;
+    }
+
+    public void setChkNoFE(String chkNoFE) {
+        this.chkNoFE = chkNoFE;
+    }
+
+    public String getChkLGE() {
+        return chkLGE;
+    }
+
+    public void setChkLGE(String chkLGE) {
+        this.chkLGE = chkLGE;
+    }
+
+    public String getChkBC() {
+        return chkBC;
+    }
+
+    public void setChkBC(String chkBC) {
+        this.chkBC = chkBC;
+    }
+
+    public String getChkABC() {
+        return chkABC;
+    }
+
+    public void setChkABC(String chkABC) {
+        this.chkABC = chkABC;
+    }
+
+    public String getChkCO() {
+        return chkCO;
+    }
+
+    public void setChkCO(String chkCO) {
+        this.chkCO = chkCO;
+    }
+
+    public String getChkD() {
+        return chkD;
+    }
+
+    public void setChkD(String chkD) {
+        this.chkD = chkD;
+    }
+
+    public String getChkK() {
+        return chkK;
+    }
+
+    public void setChkK(String chkK) {
+        this.chkK = chkK;
+    }
+
+    public String getChkDontNo() {
+        return chkDontNo;
+    }
+
+    public void setChkDontNo(String chkDontNo) {
+        this.chkDontNo = chkDontNo;
+    }
+
+    public String getTxtOther() {
+        return txtOther;
+    }
+
+    public void setTxtOther(String txtOther) {
+        this.txtOther = txtOther;
+    }
+
+    public String getChkLessMD() {
+        return chkLessMD;
+    }
+
+    public void setChkLessMD(String chkLessMD) {
+        this.chkLessMD = chkLessMD;
+    }
+
+    public String getChkMoreThaOnYMD() {
+        return chkMoreThaOnYMD;
+    }
+
+    public void setChkMoreThaOnYMD(String chkMoreThaOnYMD) {
+        this.chkMoreThaOnYMD = chkMoreThaOnYMD;
+    }
+
+    public String getChkMoreThnTwoYMD() {
+        return chkMoreThnTwoYMD;
+    }
+
+    public void setChkMoreThnTwoYMD(String chkMoreThnTwoYMD) {
+        this.chkMoreThnTwoYMD = chkMoreThnTwoYMD;
+    }
+
+    public String getChkDontNoMD() {
+        return chkDontNoMD;
+    }
+
+    public void setChkDontNoMD(String chkDontNoMD) {
+        this.chkDontNoMD = chkDontNoMD;
+    }
+
+    public String getChkYesAS() {
+        return chkYesAS;
+    }
+
+    public void setChkYesAS(String chkYesAS) {
+        this.chkYesAS = chkYesAS;
+    }
+
+    public String getChkNoAS() {
+        return chkNoAS;
+    }
+
+    public void setChkNoAS(String chkNoAS) {
+        this.chkNoAS = chkNoAS;
+    }
+
+    public String getChkYesSW() {
+        return chkYesSW;
+    }
+
+    public void setChkYesSW(String chkYesSW) {
+        this.chkYesSW = chkYesSW;
+    }
+
+    public String getChkNoSW() {
+        return chkNoSW;
+    }
+
+    public void setChkNoSW(String chkNoSW) {
+        this.chkNoSW = chkNoSW;
+    }
+
+    public String getChkDontNoSW() {
+        return chkDontNoSW;
+    }
+
+    public void setChkDontNoSW(String chkDontNoSW) {
+        this.chkDontNoSW = chkDontNoSW;
+    }
+
+    public String getChkLessLMD() {
+        return chkLessLMD;
+    }
+
+    public void setChkLessLMD(String chkLessLMD) {
+        this.chkLessLMD = chkLessLMD;
+    }
+
+    public String getChkMoreThaOnYLMD() {
+        return chkMoreThaOnYLMD;
+    }
+
+    public void setChkMoreThaOnYLMD(String chkMoreThaOnYLMD) {
+        this.chkMoreThaOnYLMD = chkMoreThaOnYLMD;
+    }
+
+    public String getChkMoreThnTwoYLMD() {
+        return chkMoreThnTwoYLMD;
+    }
+
+    public void setChkMoreThnTwoYLMD(String chkMoreThnTwoYLMD) {
+        this.chkMoreThnTwoYLMD = chkMoreThnTwoYLMD;
+    }
+
+    public String getChkDontNoLMD() {
+        return chkDontNoLMD;
+    }
+
+    public void setChkDontNoLMD(String chkDontNoLMD) {
+        this.chkDontNoLMD = chkDontNoLMD;
+    }
+
+    public String getChkYesHS() {
+        return chkYesHS;
+    }
+
+    public void setChkYesHS(String chkYesHS) {
+        this.chkYesHS = chkYesHS;
+    }
+
+    public String getChkNoHS() {
+        return chkNoHS;
+    }
+
+    public void setChkNoHS(String chkNoHS) {
+        this.chkNoHS = chkNoHS;
+    }
+
+    public String getChkYesSWT() {
+        return chkYesSWT;
+    }
+
+    public void setChkYesSWT(String chkYesSWT) {
+        this.chkYesSWT = chkYesSWT;
+    }
+
+    public String getChkNoSWT() {
+        return chkNoSWT;
+    }
+
+    public void setChkNoSWT(String chkNoSWT) {
+        this.chkNoSWT = chkNoSWT;
+    }
+
+    public String getChkDontNoSWT() {
+        return chkDontNoSWT;
+    }
+
+    public void setChkDontNoSWT(String chkDontNoSWT) {
+        this.chkDontNoSWT = chkDontNoSWT;
+    }
+
+    public String getChkLessLMDT() {
+        return chkLessLMDT;
+    }
+
+    public void setChkLessLMDT(String chkLessLMDT) {
+        this.chkLessLMDT = chkLessLMDT;
+    }
+
+    public String getChkMoreThaOnYLMDT() {
+        return chkMoreThaOnYLMDT;
+    }
+
+    public void setChkMoreThaOnYLMDT(String chkMoreThaOnYLMDT) {
+        this.chkMoreThaOnYLMDT = chkMoreThaOnYLMDT;
+    }
+
+    public String getChkMoreThnTwoYLMDT() {
+        return chkMoreThnTwoYLMDT;
+    }
+
+    public void setChkMoreThnTwoYLMDT(String chkMoreThnTwoYLMDT) {
+        this.chkMoreThnTwoYLMDT = chkMoreThnTwoYLMDT;
+    }
+
+    public String getChkDontNoLMDT() {
+        return chkDontNoLMDT;
+    }
+
+    public void setChkDontNoLMDT(String chkDontNoLMDT) {
+        this.chkDontNoLMDT = chkDontNoLMDT;
+    }
+
+    public String getChkYesCS() {
+        return chkYesCS;
+    }
+
+    public void setChkYesCS(String chkYesCS) {
+        this.chkYesCS = chkYesCS;
+    }
+
+    public String getChkNoCS() {
+        return chkNoCS;
+    }
+
+    public void setChkNoCS(String chkNoCS) {
+        this.chkNoCS = chkNoCS;
+    }
+
+    public String getChkYesSWTH() {
+        return chkYesSWTH;
+    }
+
+    public void setChkYesSWTH(String chkYesSWTH) {
+        this.chkYesSWTH = chkYesSWTH;
+    }
+
+    public String getChkNoSWTH() {
+        return chkNoSWTH;
+    }
+
+    public void setChkNoSWTH(String chkNoSWTH) {
+        this.chkNoSWTH = chkNoSWTH;
+    }
+
+    public String getChkDontNoSWTH() {
+        return chkDontNoSWTH;
+    }
+
+    public void setChkDontNoSWTH(String chkDontNoSWTH) {
+        this.chkDontNoSWTH = chkDontNoSWTH;
+    }
+
+    public String getChkLessLMDTH() {
+        return chkLessLMDTH;
+    }
+
+    public void setChkLessLMDTH(String chkLessLMDTH) {
+        this.chkLessLMDTH = chkLessLMDTH;
+    }
+
+    public String getChkMoreThaOnYLMDTH() {
+        return chkMoreThaOnYLMDTH;
+    }
+
+    public void setChkMoreThaOnYLMDTH(String chkMoreThaOnYLMDTH) {
+        this.chkMoreThaOnYLMDTH = chkMoreThaOnYLMDTH;
+    }
+
+    public String getChkMoreThnTwoYLMDTH() {
+        return chkMoreThnTwoYLMDTH;
+    }
+
+    public void setChkMoreThnTwoYLMDTH(String chkMoreThnTwoYLMDTH) {
+        this.chkMoreThnTwoYLMDTH = chkMoreThnTwoYLMDTH;
+    }
+
+    public String getChkDontNoLMDTH() {
+        return chkDontNoLMDTH;
+    }
+
+    public void setChkDontNoLMDTH(String chkDontNoLMDTH) {
+        this.chkDontNoLMDTH = chkDontNoLMDTH;
+    }
+
+    public String getChkYesWTF() {
+        return chkYesWTF;
+    }
+
+    public void setChkYesWTF(String chkYesWTF) {
+        this.chkYesWTF = chkYesWTF;
+    }
+
+    public String getChkNoWTF() {
+        return chkNoWTF;
+    }
+
+    public void setChkNoWTF(String chkNoWTF) {
+        this.chkNoWTF = chkNoWTF;
+    }
+
+    public String getChkDontNoWTF() {
+        return chkDontNoWTF;
+    }
+
+    public void setChkDontNoWTF(String chkDontNoWTF) {
+        this.chkDontNoWTF = chkDontNoWTF;
+    }
+
+    public String getTxtCommnts() {
+        return txtCommnts;
+    }
+
+    public void setTxtCommnts(String txtCommnts) {
+        this.txtCommnts = txtCommnts;
+    }
+}
